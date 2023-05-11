22486354aa40c6ccab5633c27278e39eb8f4182a
七画一只妖 <1157529280@qq.com>
2023-05-11 15:20:32 +0800

    #BOKE-22# 主界面-素材浏览-点赞功能

--- higanbana-sorrow/src/main/java/top/twip/higanbana/controller/MemeController.java
+++ higanbana-sorrow/src/main/java/top/twip/higanbana/controller/MemeController.java
@@ -2,12 +2,15 @@ package top.twip.higanbana.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import top.twip.common.constant.CurrencyConstants;
 import top.twip.common.response.DataFactory;
 import top.twip.common.response.SimpleData;
 import top.twip.higanbana.service.meme.MemeInfoService;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/higanbana/meme")
@@ -19,5 +22,22 @@ public class MemeController {
     public Object query(){
         return DataFactory.success(SimpleData.class,"查询成功")
                 .parseData(memeInfoService.query());
+    }
+
+    @GetMapping("queryLike")
+    public Object queryLike(@RequestParam("memeId") String memeId,
+                            HttpServletRequest request) {
+        String token = request.getHeader(CurrencyConstants.CURRENCY_HEADER_NAME.getValue());
+        return DataFactory.success(SimpleData.class, "操作成功")
+                .parseData(memeInfoService.queryLike(memeId, token));
+    }
+
+    @GetMapping("updateLike")
+    public Object updateLike(@RequestParam("memeId") String memeId,
+                             @RequestParam("type") String type,
+                             HttpServletRequest request) throws Exception {
+        String token = request.getHeader(CurrencyConstants.CURRENCY_HEADER_NAME.getValue());
+        memeInfoService.updateLike(memeId, type, token);
+        return DataFactory.success(SimpleData.class, "操作成功");
     }
 }
--- higanbana-sorrow/src/main/java/top/twip/higanbana/service/meme/MemeInfoService.java
+++ higanbana-sorrow/src/main/java/top/twip/higanbana/service/meme/MemeInfoService.java
@@ -1,8 +1,15 @@
 package top.twip.higanbana.service.meme;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.apache.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import top.twip.common.entity.meme.MemeInfo;
+import top.twip.common.entity.meme.MemeLikeBind;
+import top.twip.common.exception.BadRequestDataException;
+import top.twip.common.exception.DatabaseHandlerException;
+import top.twip.common.util.TokenRedisHandler;
 import top.twip.higanbana.dao.MemeInfoDao;
+import top.twip.higanbana.dao.MemeLikeDao;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -12,11 +19,65 @@ public class MemeInfoService {
     @Resource
     private MemeInfoDao memeInfoDao;
 
+    @Resource
+    private MemeLikeDao memeLikeDao;
+
+    @Resource
+    private TokenRedisHandler tokenRedisHandler;
+
     /**
      * 查所有的meme
      * @return meme列表
      */
     public List<MemeInfo> query(){
         return memeInfoDao.selectList(null);
+    }
+
+    public MemeLikeBind queryLike(String memeId, String token) {
+        String userId = tokenRedisHandler.getIdByToken(token);
+        return memeLikeDao.selectOne(new QueryWrapper<MemeLikeBind>()
+                .eq("meme_id", memeId)
+                .eq("user_id", userId)
+        );
+    }
+
+    /**
+     * 更新meme的点赞数量
+     * @param memeId ID
+     * @param type 类型：inc/des
+     * @param token TOKEN
+     */
+    public void updateLike(String memeId, String type, String token) throws Exception {
+        String userId = tokenRedisHandler.getIdByToken(token);
+
+        // 判断是否已经赞过
+        MemeLikeBind one = memeLikeDao.selectOne(new QueryWrapper<MemeLikeBind>()
+                .eq("meme_id", memeId)
+                .eq("user_id", userId)
+        );
+
+        if ("inc".equals(type)){
+            if (one != null) {
+                throw new DatabaseHandlerException("你不能重复点赞");
+            }
+            // 更新数据
+            MemeLikeBind bind = new MemeLikeBind();
+            bind.setMemeId(memeId);
+            bind.setUserId(userId);
+            memeLikeDao.insert(bind);
+            memeInfoDao.incLike(memeId);
+        } else if ("des".equals(type)) {
+            if (one == null) {
+                throw new DatabaseHandlerException("请先点赞再取消");
+            }
+            // 更新数据
+            memeLikeDao.delete(new QueryWrapper<MemeLikeBind>()
+                    .eq("meme_id", memeId)
+                    .eq("user_id", userId)
+            );
+            memeInfoDao.desLike(memeId);
+        } else {
+            throw new BadRequestDataException("你需要传入一个正确的type");
+        }
     }
 }
