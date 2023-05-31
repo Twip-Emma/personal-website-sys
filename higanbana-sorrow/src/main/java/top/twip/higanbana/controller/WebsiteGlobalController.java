d1bf252212172ed82f11151bee9c059b56d26091
七画一只妖 <1157529280@qq.com>
2023-05-31 14:27:16 +0800

    #BOKE-40# 主界面-管理界面-网站留言管理

--- higanbana-sorrow/src/main/java/top/twip/higanbana/controller/WebsiteGlobalController.java
+++ higanbana-sorrow/src/main/java/top/twip/higanbana/controller/WebsiteGlobalController.java
@@ -3,7 +3,9 @@ package top.twip.higanbana.controller;
 import org.springframework.web.bind.annotation.*;
 import top.twip.common.constant.CurrencyConstants;
 import top.twip.common.entity.blog.WebsiteMessageEntity;
+import top.twip.common.response.BaseData;
 import top.twip.common.response.DataFactory;
+import top.twip.common.response.ListData;
 import top.twip.common.response.SimpleData;
 import top.twip.higanbana.service.WebsiteCommonService;
 
@@ -22,9 +24,16 @@ public class WebsiteGlobalController {
      * @return Object 留言列表
      */
     @GetMapping("/getallmessage")
-    public Object getAllMessage() throws Exception{
-        return DataFactory.success(SimpleData.class,"查询成功")
-                .parseData(websiteCommonService.getAllMessage());
+    public Object getAllMessage(@RequestParam("page")Integer page,
+                                @RequestParam(value = "text", required = false)String text) throws Exception{
+        BaseData R = DataFactory.success(ListData.class, "查询成功")
+                .parseData(websiteCommonService.getAllMessage(page, text));
+        if (text != null && !text.isEmpty()){
+            R.parseData(websiteCommonService.getReplyCount(text));
+        } else {
+            R.parseData(websiteCommonService.getReplyCount(null));
+        }
+        return R;
     }
 
     /**
@@ -37,5 +46,31 @@ public class WebsiteGlobalController {
         String token = request.getHeader(CurrencyConstants.CURRENCY_HEADER_NAME.getValue());
         return DataFactory.success(SimpleData.class,"查询成功")
                 .parseData(websiteCommonService.addMessage(websiteMessageEntity, token));
+    }
+
+
+    /**
+     * 修改一个网站留言
+     * @param websiteMessageEntity 网站留言实体
+     */
+    @PostMapping("/updateMessage")
+    public Object updateMessage(@RequestBody WebsiteMessageEntity websiteMessageEntity,
+                             HttpServletRequest request) throws Exception{
+        String token = request.getHeader(CurrencyConstants.CURRENCY_HEADER_NAME.getValue());
+        websiteCommonService.updateMessage(websiteMessageEntity, token);
+        return DataFactory.success(SimpleData.class,"查询成功");
+    }
+
+
+    /**
+     * 删除一个网站留言
+     * @param id 网站留言ID
+     */
+    @GetMapping("/deleteMessage")
+    public Object deleteMessage(@RequestParam("id") String id,
+                                HttpServletRequest request) throws Exception{
+        String token = request.getHeader(CurrencyConstants.CURRENCY_HEADER_NAME.getValue());
+        websiteCommonService.deleteMessage(id, token);
+        return DataFactory.success(SimpleData.class,"查询成功");
     }
 }
--- higanbana-sorrow/src/main/java/top/twip/higanbana/service/WebsiteCommonService.java
+++ higanbana-sorrow/src/main/java/top/twip/higanbana/service/WebsiteCommonService.java
@@ -1,9 +1,11 @@
 package top.twip.higanbana.service;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.stereotype.Service;
 import top.twip.common.entity.blog.WebsiteMessageEntity;
 import top.twip.common.entity.user.WebsiteUserInfo;
+import top.twip.common.exception.BadRequestDataException;
 import top.twip.common.exception.DatabaseHandlerException;
 import top.twip.common.util.TokenRedisHandler;
 import top.twip.higanbana.dao.WebsiteMessageEntityDao;
@@ -33,22 +35,44 @@ public class WebsiteCommonService {
      *
      * @return List<WebsiteMessageEntity> 网站留言实体列表
      */
-    public List<WebsiteMessageEntity> getAllMessage() throws Exception {
-        try {
-            List<WebsiteMessageEntity> entities = websiteMessageEntityDao.selectList(null);
-            for (WebsiteMessageEntity o : entities) {
-                WebsiteUserInfo userInfo = websiteUserInfoDao.selectById(o.getUserId());
-                if (userInfo == null) {
-                    continue;
-                }
-                o.setAvatar(userInfo.getAvatar());
-                o.setNickname(userInfo.getNickname());
-            }
-            return entities;
-        } catch (Exception e) {
-            throw new DatabaseHandlerException(e.getMessage());
+    public List<WebsiteMessageEntity> getAllMessage(Integer page, String text) throws Exception {
+        Page<WebsiteMessageEntity> objectPage = new Page<>(page, 10);
+        QueryWrapper<WebsiteMessageEntity> wrapper = new QueryWrapper<>();
+
+        if (text != null && !"".equals(text)) {
+            wrapper.like("content", text);
         }
+
+        wrapper.orderByDesc("ctime");
+        Page<WebsiteMessageEntity> pageResult = websiteMessageEntityDao.selectPage(objectPage, wrapper);
+        List<WebsiteMessageEntity> entities = pageResult.getRecords();
+
+        for (WebsiteMessageEntity o : entities) {
+            WebsiteUserInfo userInfo = websiteUserInfoDao.selectById(o.getUserId());
+            if (userInfo == null) {
+                continue;
+            }
+            o.setAvatar(userInfo.getAvatar());
+            o.setNickname(userInfo.getNickname());
+        }
+
+        return entities;
     }
+
+
+    /**
+     * 查询所有网站评论数量
+     * @param text 文本
+     * @return Integer 数量
+     */
+    public Integer getReplyCount(String text) {
+        QueryWrapper<WebsiteMessageEntity> queryWrapper = new QueryWrapper<>();
+        if (text != null) {
+            queryWrapper.like("content", text);
+        }
+        return websiteMessageEntityDao.selectCount(queryWrapper);
+    }
+
 
 
     /**
@@ -66,5 +90,38 @@ public class WebsiteCommonService {
             throw new DatabaseHandlerException("数据库执行插入的时候出现错误力");
         }
         return input;
+    }
+
+
+    /**
+     * 修改网站留言
+     *
+     * @param input 网站留言实体
+     * @param token TOKEN
+     * @return WebsiteMessageEntity 网站留言实体
+     */
+    public void updateMessage(WebsiteMessageEntity input, String token) throws Exception {
+        if(!tokenRedisHandler.isAdmin(token)) {
+            throw new BadRequestDataException("权限不足，需要管理员权限");
+        }
+        WebsiteMessageEntity message = new WebsiteMessageEntity();
+        message.setId(input.getId());
+        message.setContent(input.getContent());
+        websiteMessageEntityDao.updateById(message);
+    }
+
+
+    /**
+     * 删除网站留言
+     *
+     * @param id 网站留言ID
+     * @param token TOKEN
+     * @return WebsiteMessageEntity 网站留言实体
+     */
+    public void deleteMessage(String id, String token) throws Exception {
+        if(!tokenRedisHandler.isAdmin(token)) {
+            throw new BadRequestDataException("权限不足，需要管理员权限");
+        }
+        websiteMessageEntityDao.deleteById(id);
     }
 }
